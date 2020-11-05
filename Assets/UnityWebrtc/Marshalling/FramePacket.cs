71d0684ff318c305cfb676b388c8bd2618b38928
mokuzaru <kenny.otoya@pucp.pe>
2020-11-05 00:16:38 -0500

    add asste

---
+++ Assets/UnityWebrtc/Marshalling/FramePacket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityWebrtc.Marshalling
+{
+    ///
+    /// Based on https://unitylist.com/p/cxl/Web-Rtc-Unity-Plugin-Sample
+    public class FramePacket
+    {
+        public FramePacket(int bufsize)
+        {
+            _buffer = new byte[bufsize];
+        }
+
+        public int width;
+        public int height;
+        private byte[] _buffer;
+        public byte[] Buffer
+        {
+            get { return _buffer; }
+        }
+
+        public override string ToString()
+        {
+            return "FramePacket width, height=(" + width + "," + height + ") buffer size:" + _buffer.Length;
+        }
+    }
+}
